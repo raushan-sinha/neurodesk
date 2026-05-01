c47b1440c93124b08a4977255d6e338b01658a87
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-01 12:42:39 +0530

    added component & logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-
+import { Navbar } from "./components/layout"
+// import { ChatBox } from "./components/ui"
+import useTheme from "./hooks/useTheme"
 
 const App = () => {
-  return (
-    <h1 className='text-3xl font-bold underline'>Hello World</h1>
-  )
+    const { theme } = useTheme()
+
+    return (
+        <>
+            <div className={`min-h-screen p-4 ${theme === 'light' ? 'bg-[#FAFAFA] text-black' : 'bg-[#0B0F19] text-white'}`}>
+                <header>
+                    <Navbar />
+                </header>
+
+                <main className="p-4">
+                    {/* <ChatBox /> */}
+                </main>
+            </div>
+        </>
+    )
 }
 
 export default App
